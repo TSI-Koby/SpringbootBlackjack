ec5dfd2d0e29bf1d13f1d2c754a0902caa880e62
Toby Whitehead <whiteheadt99@gmail.com>
2021-09-21 16:21:44 +0100

    added springboot framework

---
+++ src/main/java/com/example/blackjackspringboot/BlackjackspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackjackspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackjackspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackjackspringbootApplication.class, args);
+	}
+
+}
